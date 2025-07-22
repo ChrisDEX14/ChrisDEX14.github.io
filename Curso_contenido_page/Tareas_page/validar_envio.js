3d6a24b018860853f68e4f6e3f9cfa0b4509026e
ChrisDEX14 <166079010+ChrisDEX14@users.noreply.github.com>
2025-07-21 20:49:09 -0400

    Add files via upload

---
+++ Curso_contenido_page/Tareas_page/validar_envio.js
@@ -0,0 +1,8 @@
+        function tareaEnviar(){
+
+        let archivo=document.getElementById("Arch");
+
+        if(archivo.files.length > 0){
+            alert("la tarea fue enviada exitosamente")
+        }
+    }
---
+++ registro/validar_registro_script.js
@@ -0,0 +1,26 @@
+/*Esta funcion me valida si los campos del formulario de registro estan llenos*/
+
+
+function registro(){
+
+
+/* Entonces lo que hice fue asignarles una variable a cada campo de mi registro, aplicando
+    document.getElementById (esto para tener capturar los valores introducidos en mi HTML).
+    Luego en un condicional evaluo cada variable en la cual puese que si almenos un elemento cumple con que
+    este vacia ("") me salte la alerta de (los campos no estan completos!) de lo contrario 
+    si ninguna cumple con que este vacia me salta (El registro a sido exitoso) y me redirecciona a la pagina
+    de login.*/
+
+
+    let username=document.getElementById("nomb").value
+    let userLastName=document.getElementById("apelli").value
+    let email=document.getElementById("cor").value
+    let pass=document.getElementById("passw").value
+
+    if(pass=="" || email=="" || userLastName=="" || username==""){
+        alert("⚠️ Los Campos no estan completos!")
+    }else{
+        alert("El registro a sido exitoso✅")
+        window.location.href="../Login/login_page.html";
+}
+}
