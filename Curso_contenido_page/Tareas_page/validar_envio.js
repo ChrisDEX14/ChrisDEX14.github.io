51a4ba59aa94bf9f37b40643e25057e8195289de
ChrisDEX14 <166079010+ChrisDEX14@users.noreply.github.com>
2025-07-24 22:09:14 -0400

    Delete Curso_contenido_page directory

--- Curso_contenido_page/Tareas_page/validar_envio.js
+++
@@ -1,8 +0,0 @@
-        function tareaEnviar(){
-
-        let archivo=document.getElementById("Arch");
-
-        if(archivo.files.length > 0){
-            alert("la tarea fue enviada exitosamente")
-        }
-    }
